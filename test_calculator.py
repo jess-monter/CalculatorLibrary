00c6e7c57f74ec71716ee129c1574fa34d7648fd
jess <jess@sensaimetrics.io>
2020-05-27 06:15:01 -0500

    fix style

--- test_calculator.py
+++ test_calculator.py
@@ -8,7 +8,7 @@ import calculator
 class TestCalculator:
 
     def test_adition(self):
-        assert 4 == calculator.add(2,2)
+        assert 4 == calculator.add(2, 2)
 
     def test_substraction(self):
         assert 2 == calculator.subtract(4, 2)
